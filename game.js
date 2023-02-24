84029c3d5c7cc660caa2ccbe4de5bbb8cb2604f1
SirShagalot <56863638+SirShagalot@users.noreply.github.com>
2023-02-24 22:13:42 +0100

    rod med ridder animationer

--- game.js
+++ game.js
@@ -6,6 +6,10 @@ let points = 1;
 let lives = 3;
 
 function start() {
+  //aktiver basis animationerne
+  // document.querySelector("#sprite_container02").classList.add("updown");
+
+  //gør sprites klikbare
   document
     .querySelector("#sprite_container01")
     .addEventListener("click", enemy01Clicked);
@@ -35,13 +39,45 @@ function enemy01Clicked() {
 function enemy02Clicked() {
   console.log("enemy02Clicked");
 
+  // gør den ikke klikbar
   document
     .querySelector("#sprite_container02")
     .removeEventListener("click", enemy02Clicked);
 
+  //stop animation
   document.querySelector("#sprite_container02").classList.add("paused");
 
+  // fjern sprite
+  document.querySelector("#sprite02").classList.add("fade_out");
+
+  // tilføjer animation end til at kalde næste funktion....
+  document
+    .querySelector("#sprite_container02")
+    .addEventListener("animationend", enemyMoved);
+
   incrementPoints();
+}
+
+function enemyMoved() {
+  // animation end fjernes
+  document
+    .querySelector("#sprite_container02")
+    .removeEventListener("animationend", enemyMoved);
+  // fjern fade_out
+  document.querySelector("#sprite_container02").classList.remove("fade_out");
+  //fjren paused
+  document.querySelector("#sprite_container02").classList.remove("paused");
+
+  // genstart animationen
+  document.querySelector("#sprite_container02").classList.remove("updown");
+  document.querySelector("#sprite_container02").offsetDown;
+  document.querySelector("#sprite_container02").classList.add("updown");
+
+  // elementet bliver klikbart igen...
+  document
+    .querySelector("#sprite_container02")
+    .addEventListener("click", enemy02Clicked);
+  //NB virker kun 1 gang...
 }
 
 function incrementPoints() {
@@ -64,13 +100,11 @@ function friendly02Clicked() {
 }
 
 function looseLife() {
-  // if (lives == 1) {
-  //   document.querySelector("#heart_container1").classList.add("pulse");
-  // }
-
-  if (lives > 0) {
+  if (lives > 1) {
     removeHeart();
     lives--;
+  } else if (lives == 1) {
+    document.querySelector("#heart_container1").classList.add("pulse");
   } else {
     gameOver();
   }
